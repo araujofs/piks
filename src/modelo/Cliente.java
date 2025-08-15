a37953dc30336d9b79fc779300ee0f85a3e3b57d
Arthur Felipe <arthurfelipe5567@gmail.com>
2025-08-15 09:28:02 -0300

    feat: add cliente model

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -5,5 +5,40 @@
 package modelo;
 
 public class Cliente {
+  private int cpf;
+  private String nome;
+  private Conta conta;
 
+  public Cliente(Integer cpf, String nome) throws Exception {
+    if (cpf == null)
+      throw new Exception("Cliente precisa de cpf para ser criado");
+
+    if (cpf.toString().length() < 4)
+      throw new Exception("Cpf inválido");
+
+    if (nome == null)
+      throw new Exception("Cliente precisa de nome para ser criado");
+
+    if (nome.isEmpty())
+      throw new Exception("Nome inválido (vazio)");
+
+    this.cpf = cpf;
+    this.nome = nome;
+  }
+
+  public int getCpf() {
+    return cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public Conta getConta() {
+    return conta;
+  }
+
+  public void setConta(Conta conta) {    
+    this.conta = conta;
+  }
 }
