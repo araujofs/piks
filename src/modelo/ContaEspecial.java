2db1c8bbca271c938ccd34a99ca15961c5040e0b
Arthur Felipe <arthurfelipe5567@gmail.com>
2025-08-15 18:21:05 -0300

    feat: add ContaEspecial model

--- src/modelo/ContaEspecial.java
+++ src/modelo/ContaEspecial.java
@@ -5,6 +5,39 @@
 package modelo;
 
 public class ContaEspecial extends Conta {
+  private double limite;
 
+  public ContaEspecial(int id, String chave, double limite) throws Exception {
+    super(id, chave);
 
+    this.limite = limite;
+  }
+
+  public ContaEspecial(int id, String chave, double saldo, double limite) throws Exception {
+    this(id, chave, limite);
+
+    setSaldo(saldo);
+  }
+  
+  @Override
+  public void debitar(double valor) throws Exception {
+    if ((getSaldo() + limite) < valor)
+      throw new Exception("Saldo e/ou limite insuficiente");
+
+    double novoSaldo = getSaldo() - valor;
+
+    if (novoSaldo < 0) {
+      this.limite -= Math.abs(novoSaldo);
+      novoSaldo = 0;
+    }
+    setSaldo(novoSaldo);
+  }
+
+  public double getLimite() {
+    return limite;
+  }
+
+  public void setLimite(double limite) {
+    this.limite = limite;
+  }  
 }
